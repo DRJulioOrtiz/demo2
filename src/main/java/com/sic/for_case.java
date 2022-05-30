7ee3cd2bbc1a222c53e9ac4aea0eddfaeaf50439
“DRJulioOrtiz” <dr.julio.cesar.ortiz.cornejo@gmail.com>
2022-05-30 13:49:44 -0500

    first commit

---
+++ src/main/java/com/sic/for_case.java
@@ -0,0 +1,15 @@
+package com.sic;
+
+/**
+ * Hello world!
+ *
+ */
+public class for_case { 
+    public static void main(String args[]){ 
+      int num = 10; 
+      if(num>5){ 
+         num+=2; 
+      } 
+      System.out.println(num); 
+    } 
+}
